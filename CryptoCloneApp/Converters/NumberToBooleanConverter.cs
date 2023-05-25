2b8c5df8ad2699908f286bed468386cd915ad200
CristianLopes <cristianqibins@hotmail.com>
2023-05-25 12:20:01 -0300

    Adding light theme

--- CryptoCloneApp/Converters/NumberToBooleanConverter.cs
+++ CryptoCloneApp/Converters/NumberToBooleanConverter.cs
@@ -21,4 +21,3 @@ namespace CryptoCloneApp.Converters
         }
     }
 }
-
--- CryptoCloneApp/Views/Pages/ProfilePage.xaml.cs
+++ CryptoCloneApp/Views/Pages/ProfilePage.xaml.cs
@@ -6,20 +6,4 @@ public partial class ProfilePage : ContentPage
 	{
 		InitializeComponent();
 	}
-
-
-
-	private void ImageViewList_Tapped(object sender, EventArgs e)
-	{
-		//This is not better way to change DataTemplate
-		collectionViewList.IsVisible = true;
-		collectionViewGrid.IsVisible = false;
-	}
-
-	private void ImageViewGrid_Tapped(object sender, EventArgs e)
-	{
-        //This is not better way to change DataTemplate
-        collectionViewList.IsVisible = false;
-        collectionViewGrid.IsVisible = true;
-    }
 }
